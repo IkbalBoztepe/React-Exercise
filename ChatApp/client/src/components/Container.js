b7e760eec2e1b0afd59ccadb25be5e4fdebc0165
İkbal <ikbal_boztepe@hotmail.com>
2023-06-09 15:06:23 +0300

    chatapp bitti

---
+++ ChatApp/client/src/components/Container.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import ChatList from "./ChatList";
+import ChatForm from "./ChatForm";
+import { useChat } from "../context/ChatContext";
+
+import { init, subscribeChat, subscribeInitialMessages } from "../socketApi";
+
+function Container() {
+  const { setMessages } = useChat();
+
+  useEffect(() => {
+    init();
+
+    subscribeInitialMessages((messeages) => {
+      setMessages(messeages);
+    });
+
+    subscribeChat((message) => {
+      setMessages((prevState) => [...prevState, { message }]);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <ChatList />
+      <ChatForm />
+    </div>
+  );
+}
+
+export default Container;
